760a4b2fc61c3554929fbbdef8cda8a34d662d73
Sandro Gvelesiani <sandrogvelesiani99@gmail.com>
2024-03-14 12:55:33 +0400

    add infrustructure

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = ">=1.3.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.43.0"
+    }
+  }
+  cloud {
+    organization = "SaNN_159951"
+    workspaces {
+      name = "remotestate"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "1-195fb299-playground-sandbox"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "storageaccount" {
+  name                     = "stg${random_string.uniquestring.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
